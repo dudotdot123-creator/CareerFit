92dc4a17c3d49e3d72942ecd895e8fcf455ae247
dudotdot123-creator <dudotdot123@gmail.com>
2026-09-21 21:43:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // ---------- silent save to Google Sheet ----------
-const SHEET_URL = "PASTE-YOUR-URL-HERE";
-const SHEET_TOKEN = "PALITAN-MO-ITO-NG-SARILING-WORD"; // must match TOKEN in Apps Script
+const SHEET_URL = "https://script.google.com/macros/s/AKfycbyPUtXaDSE6CzviVdZ7pLwutsH9IuNNcW0l8gXJnzbXY7I3PqMaKNI_U4kHYAzLH9xgZQ/exec";
+const SHEET_TOKEN = "CareerFit"; // must match TOKEN in Apps Script
 
 function sendToSheet(d) {
-  if (!SHEET_URL || SHEET_URL.includes("PASTE-YOUR-URL")) return;
+  if (!SHEET_URL || SHEET_URL.includes("https://script.google.com/macros/s/AKfycbyPUtXaDSE6CzviVdZ7pLwutsH9IuNNcW0l8gXJnzbXY7I3PqMaKNI_U4kHYAzLH9xgZQ/exec")) return;
   try {
     fetch(SHEET_URL, {
       method: "POST",
